eedf7dbfe0f9acfa4c0b0eb1b872444aad8dfb40
Vita Chumakova <me@ezhevita.dev>
2023-12-23 03:44:35 +0400

    disable web page preview

--- SteamToolsBot/Bot.cs
+++ SteamToolsBot/Bot.cs
@@ -76,7 +76,7 @@ public class Bot
 
 				if (!string.IsNullOrEmpty(response))
 				{
-					await client.SendTextMessageAsync(message.Chat.Id, response, parseMode: ParseMode.MarkdownV2, cancellationToken: cancellationToken);
+					await client.SendTextMessageAsync(message.Chat.Id, response, parseMode: ParseMode.MarkdownV2, disableWebPagePreview: true, cancellationToken: cancellationToken);
 				}
 #pragma warning disable CA1031
 			} catch (Exception e)
