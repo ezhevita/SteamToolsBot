961a7239aadc3027edc4f24317e754f1cc78c2ca
Vita Chumakova <me@ezhevita.dev>
2024-07-25 03:11:31 +0400

    simplify parsing currency value

--- SteamToolsBot/Helpers/CurrencyJsonConverter.cs
+++ SteamToolsBot/Helpers/CurrencyJsonConverter.cs
@@ -1,23 +1,20 @@
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 
 namespace SteamToolsBot.Helpers;
 
-internal sealed partial class CurrencyJsonConverter : JsonConverter<decimal>
+internal sealed class CurrencyJsonConverter : JsonConverter<decimal>
 {
-	[GeneratedRegex(@"\$|[\u20A0-\u20CF]|(\s*pуб\.)", RegexOptions.Compiled | RegexOptions.CultureInvariant)]
-	private static partial Regex CurrencyRegex();
-
 	public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
 		return reader.TokenType switch
 		{
-			JsonTokenType.String => decimal.Parse(
-				CurrencyRegex().Replace(reader.GetString()!.Replace(" or more", "", StringComparison.Ordinal), "")
-					.Replace(',', '.'), CultureInfo.InvariantCulture),
+			JsonTokenType.String => uint.Parse(
+					new string(reader.GetString()!.Where(char.IsAsciiDigit).ToArray()), CultureInfo.InvariantCulture) /
+				(decimal) 100.0,
 			JsonTokenType.Number => reader.GetDecimal(),
 			_ => throw new NotImplementedException()
 		};
